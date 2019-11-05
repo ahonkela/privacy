6fd5ff5f95d1a38e449f20d2737f7130b740e0ec
Antti Honkela <antti.honkela@helsinki.fi>
2019-11-05 09:10:25 +0200

    logistic regression tutorial using synthetic toy data

--- tutorials/toy_lr_tutorial.py
+++ tutorials/toy_lr_tutorial.py
@@ -11,8 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Modified to use logistic regression instead of CNN
+# and synthetic data instead of MNIST by Antti Honkela, 2019
 
-"""Training a CNN on MNIST with differentially private SGD optimizer."""
+"""Training a logistic regression model with differentially private SGD optimizer."""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -22,6 +25,7 @@ from absl import app
 from absl import flags
 
 import numpy as np
+import numpy.random as npr
 import tensorflow as tf
 
 from tensorflow_privacy.privacy.analysis import privacy_ledger
@@ -29,22 +33,25 @@ from tensorflow_privacy.privacy.analysis.rdp_accountant import compute_rdp_from_
 from tensorflow_privacy.privacy.analysis.rdp_accountant import get_privacy_spent
 from tensorflow_privacy.privacy.optimizers import dp_optimizer
 
-GradientDescentOptimizer = tf.compat.v1.train.GradientDescentOptimizer
+AdamOptimizer = tf.compat.v1.train.AdamOptimizer
 
 FLAGS = flags.FLAGS
 
 flags.DEFINE_boolean(
     'dpsgd', True, 'If True, train with DP-SGD. If False, '
     'train with vanilla SGD.')
-flags.DEFINE_float('learning_rate', .15, 'Learning rate for training')
-flags.DEFINE_float('noise_multiplier', 1.1,
+flags.DEFINE_float('learning_rate', .05, 'Learning rate for training')
+flags.DEFINE_float('noise_multiplier', 2.0,
                    'Ratio of the standard deviation to the clipping norm')
 flags.DEFINE_float('l2_norm_clip', 1.0, 'Clipping norm')
-flags.DEFINE_integer('batch_size', 256, 'Batch size')
-flags.DEFINE_integer('epochs', 60, 'Number of epochs')
+flags.DEFINE_integer('batch_size', 64, 'Batch size')
+flags.DEFINE_integer('epochs', 2, 'Number of epochs')
 flags.DEFINE_integer(
-    'microbatches', 256, 'Number of microbatches '
+    'microbatches', 64, 'Number of microbatches '
     '(must evenly divide batch_size)')
+flags.DEFINE_integer('training_data_size', 2000, 'Training data size')
+flags.DEFINE_integer('test_data_size', 2000, 'Test data size')
+flags.DEFINE_integer('input_dimension', 5, 'Input dimension')
 flags.DEFINE_string('model_dir', None, 'Model directory')
 
 
@@ -69,24 +76,11 @@ class EpsilonPrintingTrainingHook(tf.estimator.SessionRunHook):
     print('For delta=1e-5, the current epsilon is: %.2f' % eps)
 
 
-def cnn_model_fn(features, labels, mode):
-  """Model function for a CNN."""
+def lr_model_fn(features, labels, mode):
+  """Model function for a LR."""
 
-  # Define CNN architecture using tf.keras.layers.
-  input_layer = tf.reshape(features['x'], [-1, 28, 28, 1])
-  y = tf.keras.layers.Conv2D(16, 8,
-                             strides=2,
-                             padding='same',
-                             activation='relu').apply(input_layer)
-  y = tf.keras.layers.MaxPool2D(2, 1).apply(y)
-  y = tf.keras.layers.Conv2D(32, 4,
-                             strides=2,
-                             padding='valid',
-                             activation='relu').apply(y)
-  y = tf.keras.layers.MaxPool2D(2, 1).apply(y)
-  y = tf.keras.layers.Flatten().apply(y)
-  y = tf.keras.layers.Dense(32, activation='relu').apply(y)
-  logits = tf.keras.layers.Dense(10).apply(y)
+  # Define logistic regression model using tf.keras.layers.
+  logits = tf.keras.layers.Dense(2).apply(features['x'])
 
   # Calculate loss as a vector (to support microbatches in DP-SGD).
   vector_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
@@ -99,14 +93,14 @@ def cnn_model_fn(features, labels, mode):
 
     if FLAGS.dpsgd:
       ledger = privacy_ledger.PrivacyLedger(
-          population_size=60000,
-          selection_probability=(FLAGS.batch_size / 60000))
+          population_size=FLAGS.training_data_size,
+          selection_probability=(FLAGS.batch_size / FLAGS.training_data_size))
 
-      # Use DP version of GradientDescentOptimizer. Other optimizers are
+      # Use DP version of AdamOptimizer. Other optimizers are
       # available in dp_optimizer. Most optimizers inheriting from
       # tf.train.Optimizer should be wrappable in differentially private
       # counterparts by calling dp_optimizer.optimizer_from_args().
-      optimizer = dp_optimizer.DPGradientDescentGaussianOptimizer(
+      optimizer = dp_optimizer.DPAdamGaussianOptimizer(
           l2_norm_clip=FLAGS.l2_norm_clip,
           noise_multiplier=FLAGS.noise_multiplier,
           num_microbatches=FLAGS.microbatches,
@@ -117,7 +111,7 @@ def cnn_model_fn(features, labels, mode):
       ]
       opt_loss = vector_loss
     else:
-      optimizer = GradientDescentOptimizer(learning_rate=FLAGS.learning_rate)
+      optimizer = AdamOptimizer(learning_rate=FLAGS.learning_rate)
       training_hooks = []
       opt_loss = scalar_loss
     global_step = tf.compat.v1.train.get_global_step()
@@ -144,40 +138,36 @@ def cnn_model_fn(features, labels, mode):
                                       loss=scalar_loss,
                                       eval_metric_ops=eval_metric_ops)
 
+def generate_data():
+  npr.seed(4242)
+  N_train = FLAGS.training_data_size
+  N_test = FLAGS.test_data_size
+  N = N_train + N_test
+  X0 = npr.randn(N, FLAGS.input_dimension)
+  temp = X0 @ npr.randn(FLAGS.input_dimension, 1) + npr.randn(N, 1)
+  Y0 = np.round(1/(1+np.exp(-temp)))
 
-def load_mnist():
-  """Loads MNIST and preprocesses to combine training and validation data."""
-  train, test = tf.keras.datasets.mnist.load_data()
-  train_data, train_labels = train
-  test_data, test_labels = test
-
-  train_data = np.array(train_data, dtype=np.float32) / 255
-  test_data = np.array(test_data, dtype=np.float32) / 255
-
-  train_labels = np.array(train_labels, dtype=np.int32)
-  test_labels = np.array(test_labels, dtype=np.int32)
-
-  assert train_data.min() == 0.
-  assert train_data.max() == 1.
-  assert test_data.min() == 0.
-  assert test_data.max() == 1.
-  assert train_labels.ndim == 1
-  assert test_labels.ndim == 1
-
-  return train_data, train_labels, test_data, test_labels
-
+  train_X = X0[0:N_train, :]
+  test_X = X0[N_train:N, :]
+  train_Y = Y0[0:N_train, 0]
+  test_Y = Y0[N_train:N, 0]
+  train_X = np.array(train_X, dtype=np.float32)
+  test_X = np.array(test_X, dtype=np.float32)
+  train_Y = np.array(train_Y, dtype=np.int32)
+  test_Y = np.array(test_Y, dtype=np.int32)
+  return train_X, train_Y, test_X, test_Y
 
 def main(unused_argv):
-  tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
+  tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
   if FLAGS.dpsgd and FLAGS.batch_size % FLAGS.microbatches != 0:
     raise ValueError('Number of microbatches should divide evenly batch_size')
 
   # Load training and test data.
-  train_data, train_labels, test_data, test_labels = load_mnist()
+  train_data, train_labels, test_data, test_labels = generate_data()
 
   # Instantiate the tf.Estimator.
-  mnist_classifier = tf.estimator.Estimator(model_fn=cnn_model_fn,
-                                            model_dir=FLAGS.model_dir)
+  lr_classifier = tf.estimator.Estimator(model_fn=lr_model_fn,
+                                         model_dir=FLAGS.model_dir)
 
   # Create tf.Estimator input functions for the training and test data.
   train_input_fn = tf.compat.v1.estimator.inputs.numpy_input_fn(
@@ -193,15 +183,15 @@ def main(unused_argv):
       shuffle=False)
 
   # Training loop.
-  steps_per_epoch = 60000 // FLAGS.batch_size
-  for epoch in range(1, FLAGS.epochs + 1):
+  steps_per_epoch = FLAGS.training_data_size // FLAGS.batch_size / 10
+  for epoch in range(1, 10*FLAGS.epochs + 1):
     # Train the model for one epoch.
-    mnist_classifier.train(input_fn=train_input_fn, steps=steps_per_epoch)
+    lr_classifier.train(input_fn=train_input_fn, steps=steps_per_epoch)
 
     # Evaluate the model and print results
-    eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+    eval_results = lr_classifier.evaluate(input_fn=eval_input_fn)
     test_accuracy = eval_results['accuracy']
-    print('Test accuracy after %d epochs is: %.3f' % (epoch, test_accuracy))
+    print('Test accuracy after %.1f epochs is: %.3f' % (epoch/10, test_accuracy))
 
 if __name__ == '__main__':
   app.run(main)
