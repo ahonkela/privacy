7ae42f3880e1b56e2d5bdd625a4d7e5a6166afcc
Antti Honkela <antti.honkela@helsinki.fi>
2020-10-31 20:47:16 +0200

    scrap absl.flags to make flags easier to change programmatically

--- tutorials/toy_lr_tutorial.py
+++ tutorials/toy_lr_tutorial.py
@@ -23,8 +23,6 @@ from __future__ import print_function
 
 import time
 
-from absl import app
-from absl import flags
 from absl import logging
 
 import numpy as np
@@ -36,26 +34,33 @@ import tensorflow.compat.v1 as tf
 from tensorflow_privacy.privacy.analysis import compute_dp_sgd_privacy_lib
 from tensorflow_privacy.privacy.optimizers import dp_optimizer
 
-FLAGS = flags.FLAGS
 
-flags.DEFINE_boolean(
-    'dpsgd', True, 'If True, train with DP-SGD. If False, '
-    'train with vanilla SGD.')
-flags.DEFINE_float('learning_rate', .05, 'Learning rate for training')
-flags.DEFINE_float('noise_multiplier', 2.0,
-                   'Ratio of the standard deviation to the clipping norm')
-flags.DEFINE_float('l2_norm_clip', 1.0, 'Clipping norm')
-flags.DEFINE_integer('batch_size', 64, 'Batch size')
-flags.DEFINE_integer('epochs', 2, 'Number of epochs')
-flags.DEFINE_integer('training_data_size', 2000, 'Training data size')
-flags.DEFINE_integer('test_data_size', 2000, 'Test data size')
-flags.DEFINE_integer('input_dimension', 5, 'Input dimension')
-flags.DEFINE_string('model_dir', None, 'Model directory')
+FLAGS = {
+  # If True, train with DP-SGD. If False, train with vanilla SGD.
+  'dpsgd': True,
+  # Learning rate for training
+  'learning_rate': 0.05,
+  # Ratio of the standard deviation to the clipping norm
+  'noise_multiplier': 2.0,
+  # Clipping norm
+  'l2_norm_clip': 1.0,
+  # Batch size
+  'batch_size': 64,
+  # Number of epochs
+  'epochs': 2,
+  # Training data size
+  'training_data_size': 2000,
+  # Test data size
+  'test_data_size': 2000,
+  # Input dimension
+  'input_dimension': 5,
+  # Model directory for TensorFlow
+  'model_dir': None}
 
 
-def lr_model_fn(features, labels, mode):
+def lr_model_fn(features, labels, mode, params):
   """Model function for a LR."""
-
+  flags = params
   # Define logistic regression model using tf.keras.layers.
   logits = tf.keras.layers.Dense(2).apply(features['x'])
 
@@ -68,20 +73,20 @@ def lr_model_fn(features, labels, mode):
   # Configure the training op (for TRAIN mode).
   if mode == tf.estimator.ModeKeys.TRAIN:
 
-    if FLAGS.dpsgd:
+    if flags['dpsgd']:
       # Use DP version of GradientDescentOptimizer. Other optimizers are
       # available in dp_optimizer. Most optimizers inheriting from
       # tf.train.Optimizer should be wrappable in differentially private
       # counterparts by calling dp_optimizer.optimizer_from_args().
       optimizer = dp_optimizer.DPGradientDescentGaussianOptimizer(
-          l2_norm_clip=FLAGS.l2_norm_clip,
-          noise_multiplier=FLAGS.noise_multiplier,
+          l2_norm_clip=flags['l2_norm_clip'],
+          noise_multiplier=flags['noise_multiplier'],
           num_microbatches=None,
-          learning_rate=FLAGS.learning_rate)
+          learning_rate=flags['learning_rate'])
       opt_loss = vector_loss
     else:
       optimizer = tf.train.GradientDescentOptimizer(
-          learning_rate=FLAGS.learning_rate)
+          learning_rate=flags['learning_rate'])
       opt_loss = scalar_loss
 
     global_step = tf.train.get_global_step()
@@ -107,13 +112,13 @@ def lr_model_fn(features, labels, mode):
                                       eval_metric_ops=eval_metric_ops)
 
 
-def generate_data():
+def generate_data(flags):
   npr.seed(4242)
-  N_train = FLAGS.training_data_size
-  N_test = FLAGS.test_data_size
+  N_train = flags['training_data_size']
+  N_test = flags['test_data_size']
   N = N_train + N_test
-  X0 = npr.randn(N, FLAGS.input_dimension)
-  temp = X0 @ npr.randn(FLAGS.input_dimension, 1) + npr.randn(N, 1)
+  X0 = npr.randn(N, flags['input_dimension'])
+  temp = X0 @ npr.randn(flags['input_dimension'], 1) + npr.randn(N, 1)
   Y0 = np.round(1/(1+np.exp(-temp)))
 
   train_X = X0[0:N_train, :]
@@ -127,23 +132,23 @@ def generate_data():
   return train_X, train_Y, test_X, test_Y
 
 
-def main(unused_argv):
+def main(flags, data):
   logging.set_verbosity(logging.INFO)
   tf.logging.set_verbosity(tf.logging.ERROR)
 
-  # Load training and test data.
-  train_data, train_labels, test_data, test_labels = generate_data()
+  train_data, train_labels, test_data, test_labels = data
 
   # Instantiate the tf.Estimator.
   lr_classifier = tf.estimator.Estimator(model_fn=lr_model_fn,
-                                         model_dir=FLAGS.model_dir)
+                                         model_dir=flags['model_dir'],
+                                         params=flags)
 
   # Create tf.Estimator input functions for the training and test data.
   train_input_fn = tf.estimator.inputs.numpy_input_fn(
       x={'x': train_data},
       y=train_labels,
-      batch_size=FLAGS.batch_size,
-      num_epochs=FLAGS.epochs,
+      batch_size=flags['batch_size'],
+      num_epochs=flags['epochs'],
       shuffle=True)
   eval_input_fn = tf.estimator.inputs.numpy_input_fn(
       x={'x': test_data},
@@ -152,8 +157,8 @@ def main(unused_argv):
       shuffle=False)
 
   # Training loop.
-  steps_per_epoch = FLAGS.training_data_size // FLAGS.batch_size / 10
-  for epoch in range(1, 10*FLAGS.epochs + 1):
+  steps_per_epoch = flags['training_data_size'] // flags['batch_size'] / 10
+  for epoch in range(1, 10*flags['epochs'] + 1):
     start_time = time.time()
     # Train the model for one epoch.
     lr_classifier.train(
@@ -168,10 +173,10 @@ def main(unused_argv):
     print('Test accuracy after %.1f epochs is: %.3f' % (epoch/10, test_accuracy))
 
     # Compute the privacy budget expended.
-    if FLAGS.dpsgd:
-      if FLAGS.noise_multiplier > 0.0:
+    if flags['dpsgd']:
+      if flags['noise_multiplier'] > 0.0:
         eps, _ = compute_dp_sgd_privacy_lib.compute_dp_sgd_privacy(
-            FLAGS.training_data_size, FLAGS.batch_size, FLAGS.noise_multiplier, epoch/10, 1e-5)
+            flags['training_data_size'], flags['batch_size'], flags['noise_multiplier'], epoch/10, 1e-5)
         print('For delta=1e-5, the current epsilon is: %.2f' % eps)
       else:
         print('Trained with DP-SGD but with zero noise.')
@@ -180,4 +185,6 @@ def main(unused_argv):
 
 
 if __name__ == '__main__':
-  app.run(main)
+  # Generate training and test data.
+  data = generate_data(FLAGS)
+  main(FLAGS, data)
